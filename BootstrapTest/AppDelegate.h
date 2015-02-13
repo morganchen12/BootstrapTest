9717e37de49b5435b4a37f2a0cdf7e1d637b5024
Morgan Chen <morganchen12@gmail.com>
2015-02-13 11:19:14 -0800

    initial commit

---
+++ BootstrapTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BootstrapTest
+//
+//  Created by Morgan Chen on 2/13/15.
+//  Copyright (c) 2015 Morgan Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BootstrapTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BootstrapTest
+//
+//  Created by Morgan Chen on 2/13/15.
+//  Copyright (c) 2015 Morgan Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
